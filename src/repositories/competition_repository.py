484db2717589cecda36966efa3d97de253283bd9
PyryL <pyry@pyry.info>
2022-11-24 20:19:17 +0200

    fixed style errors

--- src/repositories/competition_repository.py
+++ src/repositories/competition_repository.py
@@ -1,4 +1,4 @@
-import requests
+# import requests
 import json
 from entities.competition import Competition
 
@@ -6,17 +6,15 @@ class CompetitionRepository:
     def __init__(self):
         self._base_path = "competitions"
 
-    def get_competition(self, id):
+    def get_competition(self, competition_id):
         try:
-            file = open(f"{self._base_path}/{id}.json", encoding="utf-8")
+            with open(f"{self._base_path}/{competition_id}.json", encoding="utf-8") as file:
+                obj = json.load(file)
+                return Competition.init_from_dict(obj)
         except FileNotFoundError:
             return None
 
-        obj = json.load(file)
-        file.close()
-        return Competition.init_from_dict(obj)
-
-    def set_competition(self, id, newValue):
-        obj = newValue.save_into_dict()
-        with open(f"{self._base_path}/{id}.json", "w", encoding="utf-8") as file:
+    def set_competition(self, competition_id, new_value):
+        obj = new_value.save_into_dict()
+        with open(f"{self._base_path}/{competition_id}.json", "w", encoding="utf-8") as file:
             json.dump(obj, file)
